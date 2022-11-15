9cda82a3afafcb19a232dfff57fa899ac539f441
Anthony Shellman <atshellman@gmail.com>
2022-11-15 10:36:19 -0700

    Refactor functions

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,27 @@
-export const getUrls = () => {
-  return fetch('http://localhost:3001/api/v1/urls')
-      .then(response => response.json())
+const getUrls = async (dataSetter, errorSetter) => {
+  try {
+  const response = await fetch('http://localhost:3001/api/v1/urls')
+  const data = await response.json()
+  dataSetter(data.urls)
+  } catch(error) {
+    errorSetter(error.message)
+  }
 }
+
+const postURL = async (newURLInfo) => {
+  try {
+ const response = await fetch('http://localhost:3001/api/v1/urls', {
+  method: 'POST',
+  body: JSON.stringify(newURLInfo),
+  headers: {
+  	'Content-Type': 'application/json'
+  }
+})
+  const data = await response.json()
+  return data
+  }
+  catch(error) {
+    return error.message
+   }
+  }
+export { getUrls,  postURL}
